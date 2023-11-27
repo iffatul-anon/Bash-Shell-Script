9c7dac25758c75856d24f1fdba1be34e6b27fd1b
Md. Iffatul Islam Anon <105257268+iffatul-anon@users.noreply.github.com>
2023-11-27 13:00:33 +0600

    Create secret_input.sh

---
+++ secret_input.sh
@@ -0,0 +1,13 @@
+# Write a program to take a user a secret user input, a string and a integer value and print all the values.
+#!/bin/bash
+
+read -p "Enter username: " username
+read -sp "Enter password: " password
+echo 
+
+read -p "Enter age: " age
+
+echo
+echo "Username: $username"
+echo "password $password"
+echo "Age: $age"
